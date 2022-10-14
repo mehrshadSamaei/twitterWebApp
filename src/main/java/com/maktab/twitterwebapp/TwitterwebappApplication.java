362abb1116196a20a321b9e480aef44807d50eaa
mehrshad <mehrshadsamaei0074@gmail.com>
2022-10-14 09:29:32 +0330

    initial commit

---
+++ src/main/java/com/maktab/twitterwebapp/TwitterwebappApplication.java
@@ -0,0 +1,13 @@
+package com.maktab.twitterwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterwebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterwebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maktab/twitterwebapp/TwitterwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maktab.twitterwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterwebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
